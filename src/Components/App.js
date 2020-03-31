fefa56c6405b26b0588c3c352b928605a105a01c
niecey0123 <s.crumpler11@gmail.com>
2020-03-31 10:31:33 -0400

    ..

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChannelsField from './ChannelsField'
+import Button from '../Container/Button'
+import TopNews from '../Container/TopNews'
+
+const App = () => (
+  <div>
+    < ChannelsField />
+    <Button />
+    <TopNews />
+  </div>
+)
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { logger } from 'redux-logger'
+import reducer from './Reducers/Index'
+import './css/styles.css'
+import App from "./Components/App"
+const store = createStore(
+reducer,
+applyMiddleware(thunk, logger)
+)
+render(
+<Provider store={store}>
+   <App />
+</Provider>,
+document.getElementById('root')
+)
